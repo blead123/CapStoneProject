851139d0b3d3c701f9db118dd6f0c33add66c7ef
psh5555 <terpis5479@gmail.com>
2021-04-11 23:23:20 +0900

    초기 커밋.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Threading;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        Mat frame;
+        Bitmap image;
+        private Thread camera;
+        int isCameraRunning = 0;
+
+        private void CaptureCamera()
+        {
+
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+            capture = new VideoCapture();
+            capture.Open(2);
+            capture.Open(1);
+            while (isCameraRunning == 1)
+            {
+                capture.Read(frame);
+                if (!frame.Empty())
+                {
+                    image = BitmapConverter.ToBitmap(frame);
+                    pictureBox1.Image = image;
+                }
+                image = null;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            frame.Dispose();
+        }
+
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("Start"))
+            {
+                CaptureCamera();
+                button1.Text = "Stop";
+                isCameraRunning = 1;
+            }
+            else
+            {
+                if (capture.IsOpened())
+                {
+                    capture.Release();
+                }
+
+                button1.Text = "Start";
+                isCameraRunning = 0;
+            }
+        }
+
+
+    }
+}
